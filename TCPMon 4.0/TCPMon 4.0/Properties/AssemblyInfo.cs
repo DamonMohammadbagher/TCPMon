839d9a1f65048babb37750dae3ef4d01e7820529
Damon Mohammadbagher <DamonMohammadbagher@users.noreply.github.com>
2021-06-04 11:49:13 +0000

    Add files via upload

---
+++ TCPMon 4.0/TCPMon 4.0/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TCPMon 4.0")]
+[assembly: AssemblyDescription("TCP Monitor 4.0   .NET Framework 2.0 , Published by Damon Mohammadbagher , Email: , Windows Platform Support: Windows 7 x64 and Win 8.1 OR Windows XP and Windows 2003 Server")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Winteacher.com")]
+[assembly: AssemblyProduct("TCPMon 4.0")]
+[assembly: AssemblyCopyright("Copyright ©  2009-2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e889ca3-aa55-4a7f-b238-3eb83aed856b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("4.0.0.1")]
+[assembly: AssemblyFileVersion("4.0.0.1")]
